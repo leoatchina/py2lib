6f82f7677e9e2c863435fcf1081f7edb7e4d8f8b
taotao <taotao@myhexin.com>
2021-03-02 10:24:50 +0800

    add -A

---
+++ sync.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : sync.py
+# Author            : taotao <taotao@myhexin.com>
+# Date              : 2021.01.14
+# Last Modified Date: 2021.01.14
+# Last Modified By  : taotao <taotao@myhexin.com>
+
+import os
+
+os.environ['PATH'] = r"d:\mingw\bin" + ';' + os.environ['PATH']
+
+cmd = (
+    r"d:\Anaconda3\python compile_py.py "
+    r"-s "
+    r"-d .\check "
+    r"-o d:\test "
+)
+print(cmd)
+os.system(cmd)
+
